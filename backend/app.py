7752f9c56ef48a71de1d35c914fc487efa87d4a1
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-14 22:45:55 +0900

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/api", methods=["POST"])
+def api():
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
