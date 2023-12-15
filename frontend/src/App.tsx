fba1b36222aa31689af65d37e65fd357f889181d
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-15 23:27:38 +0900

    tailwindを導入した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-    return <></>
+import './index.css'
+
+const App = () => {
+    return (
+        <>
+            <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        </>
+    )
 }
 
 export default App
