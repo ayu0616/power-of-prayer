e18f6314cd5b7eaef10af27927e533e242665515
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-15 21:00:07 +0900

    Dockerで開発環境を立ち上げられるようにした

--- backend/app.py
+++ backend/app.py
@@ -11,4 +11,4 @@ def api():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000, host="0.0.0.0")
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/power-of-prayer/',
+    plugins: [react()],
+    base: '/power-of-prayer/',
+    server: {
+        host: '0.0.0.0',
+    },
 })
