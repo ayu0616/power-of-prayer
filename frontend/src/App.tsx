bf55e0f6b3bf377d8329ac53eb8e77435688af32
ayu0616 <ayumuogawa0616@gmail.com>
2023-12-16 12:50:01 +0900

    画面の高さを設定した

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
     }
 
     return (
-        <div className='bg-slate-50 p-8'>
+        <div className='bg-slate-50 p-8 h-[100svh]'>
             <div className='flex flex-col gap-6'>
                 <div className='flex flex-col gap-4'>
                     <div className='flex flex-col gap-4'>
